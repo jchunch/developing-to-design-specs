5920e524a387e2288d242658440f74afd88b432e
John Hunchar <john@snipsnap.it>
2016-02-23 08:57:07 -0500

    Updated NetworkUtils.

--- dynamic-feed/app/src/main/java/com/jchunch/dynamicfeed/item/TileItemUtils.java
+++ dynamic-feed/app/src/main/java/com/jchunch/dynamicfeed/item/TileItemUtils.java
@@ -2,11 +2,9 @@ package com.jchunch.dynamicfeed.item;
 
 import android.text.TextUtils;
 
-import com.google.gson.Gson;
 import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-import com.google.gson.JsonSyntaxException;
 import com.jchunch.dynamicfeed.item.large.LargeTileItem;
 import com.jchunch.dynamicfeed.item.regular.RegularTileItem;
 import com.jchunch.dynamicfeed.item.small.SmallTileItem;
@@ -14,6 +12,7 @@ import com.jchunch.dynamicfeed.log.LogUtil;
 import com.jchunch.dynamicfeed.model.LargeTile;
 import com.jchunch.dynamicfeed.model.RegularTile;
 import com.jchunch.dynamicfeed.model.SmallTile;
+import com.jchunch.dynamicfeed.network.NetworkUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,58 +28,26 @@ public class TileItemUtils {
     private static final String KEY_VALUE_HEADER = "header";
     private static final String KEY_VALUE_IMAGE_URL = "imageUrl";
     private static final String KEY_VALUE_TYPE = "type";
+
     private static final String TYPE_VALUE_LARGE = "large";
     private static final String TYPE_VALUE_REGULAR = "regular";
     private static final String TYPE_VALUE_SMALL = "small";
 
     public static LargeTileItem buildLargeTileItem(JsonObject jsonObject) {
-
-        String header = null;
-        String body = null;
-        String imageUrl = null;
-
-        if (jsonObject != null) {
-            header = jsonObject.get(KEY_VALUE_HEADER).getAsString();
-            body = jsonObject.get(KEY_VALUE_BODY).getAsString();
-            imageUrl = jsonObject.get(KEY_VALUE_IMAGE_URL).getAsString();
-        }
-
-        LargeTile largeTile = new LargeTile(header, body, imageUrl);
-
+        String jsonString = NetworkUtils.convertObjectToJsonString(jsonObject);
+        LargeTile largeTile = NetworkUtils.fromJson(jsonString, LargeTile.class);
         return new LargeTileItem(largeTile);
     }
 
     public static RegularTileItem buildRegularTileItem(JsonObject jsonObject) {
-
-        String header = null;
-        String body = null;
-        String imageUrl = null;
-
-        if (jsonObject != null) {
-            header = jsonObject.get(KEY_VALUE_HEADER).getAsString();
-            body = jsonObject.get(KEY_VALUE_BODY).getAsString();
-            imageUrl = jsonObject.get(KEY_VALUE_IMAGE_URL).getAsString();
-        }
-
-        RegularTile regularTile = new RegularTile(header, body, imageUrl);
-
+        String jsonString = NetworkUtils.convertObjectToJsonString(jsonObject);
+        RegularTile regularTile = NetworkUtils.fromJson(jsonString, RegularTile.class);
         return new RegularTileItem(regularTile);
     }
 
     public static SmallTileItem buildSmallTileItem(JsonObject jsonObject) {
-
-        String header = null;
-        String body = null;
-        String imageUrl = null;
-
-        if (jsonObject != null) {
-            header = jsonObject.get(KEY_VALUE_HEADER).getAsString();
-            body = jsonObject.get(KEY_VALUE_BODY).getAsString();
-            imageUrl = jsonObject.get(KEY_VALUE_IMAGE_URL).getAsString();
-        }
-
-        SmallTile smallTile = new SmallTile(header, body, imageUrl);
-
+        String jsonString = NetworkUtils.convertObjectToJsonString(jsonObject);
+        SmallTile smallTile = NetworkUtils.fromJson(jsonString, SmallTile.class);
         return new SmallTileItem(smallTile);
     }
 
@@ -88,14 +55,7 @@ public class TileItemUtils {
         List<TileItem> tileItems = new ArrayList<TileItem>();
 
         if (!TextUtils.isEmpty(jsonString)) {
-            JsonObject jsonObject = null;
-
-            try {
-                jsonObject = new Gson().fromJson(jsonString, JsonObject.class);
-            } catch (JsonSyntaxException e) {
-                LogUtil.w(TAG, e.getMessage());
-            }
-
+            JsonObject jsonObject = NetworkUtils.fromJson(jsonString, JsonObject.class);
             if (jsonObject != null) {
                 JsonArray jsonArray = jsonObject.getAsJsonArray(KEY_VALUE_FEED);
                 if (jsonArray != null) {
--- dynamic-feed/app/src/main/java/com/jchunch/dynamicfeed/network/NetworkUtils.java
+++ dynamic-feed/app/src/main/java/com/jchunch/dynamicfeed/network/NetworkUtils.java
@@ -1,8 +1,11 @@
 package com.jchunch.dynamicfeed.network;
 
 import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonSyntaxException;
+import com.jchunch.dynamicfeed.log.LogUtil;
 
 import retrofit2.Response;
 
@@ -10,6 +13,7 @@ import retrofit2.Response;
  * Created by jchunch on 2/22/16.
  */
 public class NetworkUtils {
+    private static final String TAG = NetworkUtils.class.getName();
 
     public static String convertObjectToJsonString(Object objectToConvert) {
         GsonBuilder gsonBuilder = new GsonBuilder()
@@ -17,6 +21,18 @@ public class NetworkUtils {
                 .serializeNulls();
 
         return gsonBuilder.create().toJson(objectToConvert);
+    }
+
+    public static <T> T fromJson(String jsonString, Class<T> classType) {
+        if (jsonString != null) {
+            try {
+                return new Gson().fromJson(jsonString, classType);
+            } catch (JsonSyntaxException e) {
+                LogUtil.w(TAG, "fromJson: " + e.getMessage());
+            }
+        }
+
+        return null;
     }
 
     public static String getResponseBodyJson(Response<JsonElement> response) {
